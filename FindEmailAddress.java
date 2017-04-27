b1d5a4c74db3997d3a53ec02e4083b808b75ccfa
xianghailiu <xianghailiu@yahoo.com>
2017-04-27 02:29:17 -0400

    Add files via upload
    Updated the implementation based on review feedback.

--- FindEmailAddress.java
+++ FindEmailAddress.java
@@ -3,125 +3,142 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
- 
+
 public class FindEmailAddress {
 	// Email Address Pattern
 	String pattern = "\\b[a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z0-9.-]+\\b";
 	// embedded link pattern <a href="....">...</a>
 	String anchorRegex = "(?s)<\\s*a\\s+.*?href\\s*=\\s*['\"]([^\\s>]*)['\"]";
 	URL url; // URL Instance Variable
-	StringBuilder contents = new StringBuilder();; // Stores our URL Contents
-	Set<String> emailAddresses = new HashSet<>(); // Contains unique email addresses
-	HashMap<Integer, URL> hmap = new HashMap<Integer, URL>(); //Contains unique urls
-	static int index = 0;
+	String urlString;
+	static Set<String> emailAddresses = new HashSet<>(); // Contains unique email addresses
+	Set<String> domainUrls = new HashSet<>(); // Contains unique urls
+	String wwwdomain;
 
 	FindEmailAddress(String url) {
-		//build a valid URL
-		String fullUrl = "http://" + url;
-		try {
-			this.url = new URL(fullUrl); // Initializing URL object
-		} catch (MalformedURLException ex) {
-			System.out.println("Please specify a valid website, for example: www.google.com or google.com");
-			System.exit(1);
-		}
+		this.urlString = url;
+		int wwwIndex = this.urlString.indexOf("www.");
+		this.wwwdomain = this.urlString.substring(( wwwIndex < 0)? 0 : wwwIndex);
 	}
 
 	/**
-	 * Read web contents line by line, stores the contents
-	 * If current web page contains discoverable pages, follow the links to get the contents
+	 * Read web contents line by line If current line contains an email address,
+	 * print out the email address If current line contains discoverable page
+	 * link, follow the link to get the contents
 	 * 
-	 * @param mUrl
+	 * @param mUrlString
 	 */
-	public void readContents(URL mUrl) {
+	public void readContents(String mUrlString) {
 		try {
 			// Open Connection to URL and get stream to read
-			BufferedReader read = new BufferedReader(new InputStreamReader(mUrl.openStream()));
+			//TODO some https websites require passing certificate verify, otherwise no contents can be read
+			BufferedReader read = new BufferedReader(new InputStreamReader(getHttpUrl(mUrlString).openStream()));
+			if (read.readLine() == null) {
+				read = new BufferedReader(new InputStreamReader(getHttpsUrl(mUrlString).openStream()));
+			}
 			// Read and Save Contents to StringBuilder variable
 			String input = "";
 			while ((input = read.readLine()) != null) {
-				contents.append(input);
-				linkChecker(input);
+				if (!linkChecker(input)) {
+					this.findEmails(input);
+				}
 			}
+			read.close();
 		} catch (IOException ex) {
-			System.out.println("\tUnable to read URL due to Unknown Host..");
 		}
 	}
 
+	private URL getHttpUrl(String uri) {
+		try {
+			String fullUri = (uri.startsWith("http:") || uri.startsWith("https:")) ? uri : "http://" + uri;
+			this.url = new URL(fullUri); // Initializing URL object
+		} catch (MalformedURLException ex) {
+			System.out.println("Please specify a valid website, for example: http://www.google.com, www.google.com or google.com");
+			System.exit(1);
+		}
+		return this.url;
+	}
+
+	private URL getHttpsUrl(String uri) {
+		try {
+			String fullUri = (uri.startsWith("https:") || uri.startsWith("http:")) ? uri : "https://" + uri;
+			this.url = new URL(fullUri); // Initializing URL object
+		} catch (MalformedURLException ex) {
+			System.out.println("Please specify a valid website, for example: http://www.google.com, www.google.com or google.com");
+			System.exit(1);
+		}
+		return this.url;
+	}
+
 	/**
-	 * Check if the string content contains a page link
-	 * and follow the pages all the way down to get the contents
-	 * One link only visits once
+	 * Check if the string content contains a page link and follow the pages all
+	 * the way down to get the contents One link only visits once
 	 * 
 	 * @param pageSource
 	 */
-	private void linkChecker(String pageSource) {
+	private boolean linkChecker(String pageSource) {
 		Pattern linkPattern = Pattern.compile(anchorRegex);
 		Matcher ma = linkPattern.matcher(pageSource);
-		//Found a href string
+		// Found a href string
 		if (ma.find()) {
 			String hrefString = ma.group(1);
 			int n = hrefString.indexOf("www.");
-			//Only interested in webpages starting with www
+			// Only interested in webpages starting with www
 			if (n > 0) {
 				String embedLink = hrefString.substring(n);
-				String mUrl = "http://" + embedLink;
-				URL embedUrl;
 				try {
-					embedUrl = new URL(mUrl); // Initializing our URL object
 					// Keep track of visited URLs, only visit once
-					if (mUrl.contains(url.getHost()) && !hmap.containsValue(embedUrl)) {
-						hmap.put(index++, embedUrl);
-						readContents(embedUrl);
+					if (embedLink.contains(this.wwwdomain) && !domainUrls.contains(embedLink)) {
+						domainUrls.add(embedLink);
+						readContents(embedLink);
+						return true;
 					} else {
-						return;
+						return false;
 					}
-				} catch (MalformedURLException ex) {
+				} catch (Exception ex) {
 					System.out.println("Not a valid URL");
 					System.exit(1);
 				}
 			}
-
+			return false;
+		} else {
+			return false;
 		}
 	}
 
 	/**
 	 * Find the domain related emails from saved contents
 	 */
-	public void findEmails() {
+	private void findEmails(String contents) {
 		// Creates a Pattern
 		Pattern pat = Pattern.compile(pattern);
 		// Matches contents against the given Email Address Pattern
 		Matcher match = pat.matcher(contents);
 		// If match found, append to emailAddresses
 		while (match.find()) {
-			//find domain related emails
+			// find domain related emails
 			String hostHame = url.getHost();
 			String domainName = hostHame.startsWith("www.") ? hostHame.substring(4) : hostHame;
 			if (match.group().contains(domainName)) {
+				if (!emailAddresses.contains(match.group())) {
+					if (emailAddresses.size() == 0) {
+						System.out.println("Found these email addresses: ");
+					}
+					System.out.println(match.group());
+				}
 				emailAddresses.add(match.group());
 			}
 		}
 	}
-
-	/**
-	 * Print out collected email addresses
-	 */
-	public void printEmailAddresses() {
-		// Check if email addresses have been extracted
-		if (emailAddresses.size() > 0) {
-			// Print out all the extracted emails
-			System.out.println("Found these email addresses: ");
-			for (String emails : emailAddresses) {
-				System.out.println(emails);
-			}
-		} else {
-			// In case, no email addresses were found
-			System.out.println("No emails were found!");
+	
+	public void checkFoundNoEmails() {
+		// In case, no email addresses were found
+		if(emailAddresses.size() == 0) {
+            System.out.println("No emails were found!");
 		}
 	}
 
@@ -133,16 +150,13 @@ public class FindEmailAddress {
 		if (args.length > 0 && args[0] != null) {
 			// Initialize Extractor with URL
 			extract = new FindEmailAddress(args[0]);
-			// Read the URL contents
-			extract.readContents(extract.url);
-			// Find all the email addresses
-			extract.findEmails();
-
-			// Otherwise normally display the email addresses
-			extract.printEmailAddresses();
+			// Read the URL contents, print email addresses
+			extract.readContents(extract.urlString);
+			extract.checkFoundNoEmails();
 		} else {
 			System.out.println("No valid arguments supplied...");
 			System.out.println("\n\tExample: java -jar FindEmailAddress.jar www.example.com");
 		}
 	}
 }
+
